26bdc662a7352c342dc32c3fb946ef42829f0522
StevenDixonDev <StevenDixon1128@gmail.com>
2019-06-27 16:09:37 -0400

    updated comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -50,14 +50,15 @@
 // 
 
 // Define the armies and their stats
+// Balancing the armies will be the hardest part
 const armies = [
   {
     name: 'Alliance Army',
-    power: 0,
-    retaliation: 0,
-    attacks: 0,
-    maxHp: 0,
-    evasion: 0,
+    power: 10,
+    retaliation: 10,
+    attacks: 2,
+    maxHp: 50,
+    evasion: 5,
     //Madines Rules: deals double damage when hp is low
     special:  function(){},
     alignment: "Republic",
@@ -68,11 +69,11 @@ const armies = [
   },
   {
     name: 'Droid Army',
-    power: 0,
-    retaliation: 0,
-    attacks: 0,
-    maxHp: 0,
-    evasion: 0,
+    power: 10,
+    retaliation: 10,
+    attacks: 2,
+    maxHp: 50,
+    evasion: 2,
     //Indomitable Force: Takes no damage for a turn after making a attack
     special:  function(){},
     alignment: "Sith",
@@ -83,12 +84,12 @@ const armies = [
   },
   {
     name: 'Gungan Grand Army',
-    power: 0,
-    retaliation: 0,
-    attacks: 0,
-    maxHp: 0,
-    evasion: 0,
-    //special: A friend in need: crickets
+    power: 10,
+    retaliation: 10,
+    attacks: 2,
+    maxHp: 50,
+    evasion: 2,
+    //special: Gungan Shields: increases evasion by *2?
     special:  function(){},
     alignment: "Republic",
     picture: {
@@ -98,11 +99,11 @@ const armies = [
   },
   {
     name: 'Imperial Army',
-    power: 0,
-    retaliation: 0,
-    attacks: 0,
-    maxHp: 0,
-    evasion: 0,
+    power: 15,
+    retaliation: 15,
+    attacks: 2,
+    maxHp: 50,
+    evasion: 2,
     //Prowess of the Sith: Lowers enemy evasion 
     special:  function(){},
     alignment: "Sith",
@@ -113,11 +114,11 @@ const armies = [
   },
   {
     name: 'Republic Army',
-    power: 0,
-    retaliation: 0,
-    attacks: 0,
-    maxHp: 0,
-    evasion: 0,
+    power: 10,
+    retaliation: 10,
+    attacks: 2,
+    maxHp: 50,
+    evasion: 2,
     //Backup: Call in an extra attack from space
     special:  function(){},
     alignment: "Sith",
@@ -128,9 +129,9 @@ const armies = [
   },
   {
     name: 'Rebel Army',
-    power: 0,
-    retaliation: 0,
-    attacks: 0,
+    power: 10,
+    retaliation: 10,
+    attacks: 2,
     maxHp: 0,
     evasion: 0,
     //Gurrella warfare: 
@@ -144,7 +145,55 @@ const armies = [
 ]
 
 
-// function to create a new army
-function createArmy(){
+// to do go find sounds for fight
+// list of sound effects
+const soundEffects = []
 
+
+
+// function to create a new army
+function createArmy(army){
+  this.hp = army.maxHp;
+  this.power = army.power;
+  this.name = army.name;
+  this.attacks = army.attacks;
+  this.evasion = army.evasion;
+  this.retaliation = army.retaliation;
+  this.special = army.special;
+  this.attack = function(evasion = 0){
+    //handles attack damage
+    let damage = 0;
+    // army attacks = to number of attacks but power is random
+    for(let i = 0; i < this.attacks; i++){
+      damage += Math.floor(Math.random()*this.power)
+    }
+    // substract other armies evasion to calculate total damage
+    damage = damage - evasion;
+    // if damage is negative set it to zero
+    if(damage < 0) damage = 0;
+    return damage;
+  }
+  this.retaliate = function(evasion = 0){
+    let damage = 0;
+    for(let i = 0; i < this.attacks; i++){
+      damage += Math.floor(Math.random()*this.retaliation)
+    }
+    damage = damage - evasion;
+    if(damage < 0) damage = this.power;
+    return damage;
+  }
+  this.levelUp = function(){
+    // when player levels up 
+    this.attacks+=1;
+  }
+  this.useSpecial = function(){
+    this.special();
+  }
 }
+
+
+let myArmy = new createArmy(armies[1]);
+
+console.log(myArmy)
+
+console.log(myArmy.attack(5))
